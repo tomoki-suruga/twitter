5cc8bab60f9d3348a2043e8eb881ed823daf6cf3
tomoki-suruga <tomoki.s.0927@gmail.com>
2016-09-01 15:42:14 +0900

    途中

--- dbconnect.php
+++ dbconnect.php
@@ -1,5 +1,5 @@
 <?php
-$db = mysqli_connect('localhost', 'root', '', 'mini_bbs') or die(mysqli_connect_error());
+$db = mysqli_connect('localhost','topia_tomoki02','shinjuku01','mini_bbs') or die(mysqli_connect_error());
 mysqli_set_charset($db, 'utf8');
 
 ?>
